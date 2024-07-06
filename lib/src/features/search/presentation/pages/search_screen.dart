62ecc171012e481332afb4452d694b59bce48de5
youssef <hb009501@gmail.com>
2024-07-06 19:56:31 +0300

    search design is ready

--- lib/src/features/search/presentation/pages/search_screen.dart
+++ lib/src/features/search/presentation/pages/search_screen.dart
@@ -1,25 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Social Media App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SearchUserPage(),
-    );
-  }
-}
 
 class SearchUserPage extends StatefulWidget {
   @override
@@ -27,92 +6,22 @@ class SearchUserPage extends StatefulWidget {
 }
 
 class _SearchUserPageState extends State<SearchUserPage> {
-  final TextEditingController _searchController = TextEditingController();
-  List<DocumentSnapshot> _searchResults = [];
-  bool _isLoading = false;
-
-  void _searchUsers(String query) async {
-    if (query.isEmpty) {
-      setState(() {
-        _searchResults = [];
-      });
-      return;
-    }
-
-    setState(() {
-      _isLoading = true;
-    });
-
-    try {
-      final results = await FirebaseFirestore.instance
-          .collection('users')
-          .where('name', isGreaterThanOrEqualTo: query)
-          .where('name', isLessThanOrEqualTo: query + '\uf8ff')
-          .get();
-
-      setState(() {
-        _searchResults = results.docs;
-      });
-    } catch (e) {
-      // Handle error
-      print(e);
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
             TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Search by username',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    _searchUsers(_searchController.text);
-                  },
-                ),
-              ),
-              onChanged: (value) {
-                _searchUsers(value);
-              },
-            ),
-            SizedBox(height: 20),
-            _isLoading
-                ? Center(child: CircularProgressIndicator())
-                : Expanded(
-              child: ListView.builder(
-                itemCount: _searchResults.length,
-                itemBuilder: (context, index) {
-                  final user = _searchResults[index].data() as Map<String, dynamic>;
-                  return ListTile(
-                    title: Text(user['username']),
-                    subtitle: Text(user['email']),
-                    onTap: () {
-                      // Handle user selection
-                    },
-                  );
-                },
-              ),
-            ),
+                decoration: InputDecoration(
+              hintText: "Enter user name",
+              border: OutlineInputBorder(),
+            )),
           ],
         ),
-      ),
+      )),
     );
   }
 }
